d8e7bfaed4dcb6e8437c462635aaed51ffcd819c
Kareem Hegazy <kthegazy2002@gmail.com>
2023-09-24 01:27:23 -0400

    Changed to new database

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -60,6 +60,7 @@ export default NextAuth({
     signIn: '/auth',
   },
   debug: process.env.NODE_ENV === 'development',
+  adapter: PrismaAdapter(prismadb),
   session: {
     strategy: 'jwt',
   },
