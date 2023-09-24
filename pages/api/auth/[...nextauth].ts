fcc9d5f602d4de3100dabfc13ad761c7e705f9a8
Kareem Hegazy <kthegazy2002@gmail.com>
2023-09-24 00:25:30 -0400

    Removed .env file from repo

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,10 +2,23 @@ import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 import { compare } from 'bcrypt';
 
+import GoogleProvider from 'next-auth/providers/google';
+import GitHubProvider from 'next-auth/providers/github';
+
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+
 import prismadb from '@/lib/prismadb';
 
 export default NextAuth({
   providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || ''
+    }),
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID || '',
+      clientSecret: process.env.GITHUB_SECRET || ''
+    }),
     Credentials({
       id: 'credentials',
       name: 'Credentials',
